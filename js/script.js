78c3b52c81349957e9b90912d1765ed5ac6b22d0
emy24107 <110105544+emy24107@users.noreply.github.com>
2024-03-11 12:11:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,5 +8,27 @@ fazerSorteio = () => {
 
     document.getElementById("resultado").innerHTML = sorteio;
 
-
 }
+function escolher_aba(btn){
+    for(let i = 1; i <= 4; i++){
+        let id_btn = "btn"+i;
+        document.getElementById(id_btn).style.backgroundColor = "violet"
+        document.getElementById(id_btn).disabled = false;
+    }
+   if(btn == "1"){
+    document.getElementById("btn1").style.backgroundColor = "red"
+    document.getElementById("btn1").disabled = true;
+   }
+   if(btn == "2"){
+    document.getElementById("btn2").style.backgroundColor = "red"
+    document.getElementById("btn2").disabled = true;
+   }
+   if(btn == "3"){
+    document.getElementById("btn3").style.backgroundColor = "red"
+    document.getElementById("btn3").disabled = true;
+   }
+   if(btn == "4"){
+    document.getElementById("btn4").style.backgroundColor = "red"
+    document.getElementById("btn4").disabled = true;
+   }
+    }
